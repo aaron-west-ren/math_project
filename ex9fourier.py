210cd085fdbdaa29556b3a3dd6e2411855273fc9
Aaron West <aaron.west@renenergyglobal.com>
2021-12-02 13:47:34 -0800

    modified:   ex9fourier.py

--- ex9fourier.py
+++ ex9fourier.py
@@ -18,7 +18,7 @@ nn = len(twopiece)  # to increse the number of points change n from above
 dx = (b-a)/nn
 x = np.arange(a,b,dx) # redef of x to fit new expanded function
 
-mm = 10 #  NOTE: CONTROLL n HERE
+mm = 100 #  NOTE: CONTROLL n HERE
        #
 m = np.arange(1,mm,1)  # m is an array [1, 2, 3, ... mm]
 
@@ -27,19 +27,23 @@ sum2 = 0  # no one cares about sumtwo
 
 ao = 1
 an = -.25 * (np.power(4 / (m * pi), 2) * (1 - np.cos(m * pi)))
-bn = -.25 * (-(4/(m *pi) * 4 * np.cos(n * pi)))
+bn = 4/(m * pi) * np.cos(m * pi)
+
+#bn = ((4/(m *pi) * np.cos(n * pi)))
 
 for i in m-1:
-    sum1 = sum1 + an[i] * np.cos(m[i] * pi * x)
-    sum2 = sum2 + bn[i] * np.sin(m[i] * pi * x)
+    sum1 = sum1 + an[i] * np.cos(m[i] * pi * x/4)
+    sum2 = sum2 + bn[i] * np.sin(m[i] * pi * x/4)
 
 estfx = 1 + sum1 + sum2
-
+error = abs(twopiece - estfx) # NOTE: error analisis
+sumerror = sum(error)
+print(sumerror)
 
 plt.plot(x, twopiece)
 plt.plot(x, estfx, 'r-')
 # plt.title('My Title\n' + r'$\alpha - \omega$ are LaTeX Markup')
-plt.title('faulty fourier Approximation of problem #9\n' + r'Aaron West 12/1/2021')
+plt.title('fourier Approximation of problem #9\n' + r'Aaron West 12/1/2021')
 
 ax = plt.gca()
 
