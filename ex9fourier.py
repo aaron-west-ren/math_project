83a0f0555f9f6f912c3af9d8e2fe321eebffdc9d
aaron-west-ren <aaron.west@renenergyglobal.com>
2021-12-01 18:02:19 -0800

    modified:   ex9fourier.py

--- ex9fourier.py
+++ ex9fourier.py
@@ -10,6 +10,42 @@ dx = (b-a)/n
 x = np.arange(a,b,dx)
 
 onepiece = np.piecewise(x, [x <= 0, x >= 0], [lambda x: -x, lambda x: 0])
+twopiece = np.append(onepiece,onepiece)  # double down of onepiece!!
 
-plt.plot(x, onepiece)
+a = -4
+b = 12
+nn = len(twopiece)  # to increse the number of points change n from above
+dx = (b-a)/nn
+x = np.arange(a,b,dx) # redef of x to fit new expanded function
+
+mm = 10 #  NOTE: CONTROLL n HERE
+       #
+m = np.arange(1,mm,1)  # m is an array [1, 2, 3, ... mm]
+
+sum1 = 0  # Initilize someone!!
+sum2 = 0  # no one cares about sumtwo
+
+ao = 1
+an = -.25 * (np.power(4 / (m * pi), 2) * (1 - np.cos(m * pi)))
+bn = -.25 * (-(4/(m *pi) * 4 * np.cos(n * pi)))
+print(an)
+print(bn)
+for i in m-1:
+    sum1 = sum1 + an[i] * np.cos(m[i] * pi * x)
+    sum2 = sum2 + bn[i] * np.sin(m[i] * pi * x)
+
+estfx = 1 + sum1 + sum2
+
+
+plt.plot(x, twopiece)
+plt.plot(x, estfx, 'r-')
+# plt.title('My Title\n' + r'$\alpha - \omega$ are LaTeX Markup')
+plt.title('faulty fourier Approximation of problem #9\n' + r'Aaron West 12/1/2021')
+
+ax = plt.gca()
+
+ax.set_aspect(1.0)
+
+ax.axhline(0, color='black', lw=1)
+ax.axvline(0, color='black', lw=2)
 plt.show()
